653ca9d73390c57940d4efc42b037b33d84cf40b
julietvangent <julietvangent@gmail.com>
2017-01-08 22:08:52 +0100

    final changes

--- forum/models.py
+++ forum/models.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 
 class Post(models.Model):
     author = models.ForeignKey('auth.User')
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=100)
     text = models.TextField()
     created_date = models.DateTimeField(
             default=timezone.now)
@@ -16,9 +16,6 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
-
-    def approved_comments(self):
-        return self.comments.filter(approved_comment=True)
 
 
 class DestinationsPost(Post):
@@ -41,7 +38,7 @@ class OtherPost(Post):
 
 class Comment(models.Model):
     post = models.ForeignKey('forum.Post', related_name='comments')
-    author = models.CharField(max_length=200)
+    author = models.CharField(max_length=50)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     approved_comment = models.BooleanField(default=False)
